6f6ab0384c3de7fc15ef8af4974afe8ea1c8832d
Aryan Rana <ranaryan402@gmail.com>
2026-03-20 21:03:22 +0530

    Initial commit - Expense Tracker

--- Expense-Tracker.py
+++ Expense-Tracker.py
@@ -7,57 +7,61 @@ ExpenseList = [] #where i am going to add my expense list
 print("====WELCOME🥰 TO THE EXPENSE TRACKER====")
 
 while True:
-        print("-------Overview-------")
+        print("\n-------Overview-------")
         print("1.Add Expense💲\n"
-                "2.View Your Expense💸\n"
-                "3.View Your Total🤑\n"
-                "4.Exit😶")
-        
-        choice = int(input("Enter Your Choice👉 (1-4) : "))
-
+                        "2.View Your Expense💸\n"
+                        "3.View Your Total🤑\n"
+                        "4.Exit😶")
+        try:       
+                choice = int(input("Enter Your Choice👉 (1-4) : "))
+        except ValueError:
+                print("Please enter a number only")
+                continue
 #1 Add Expense
         if choice == 1:
                 date = input("Date of Expense (YY-MM-DD) : ")
-                type = input(" Type (Fashion👔 , Sports⚽ , Education📚 , etc🎨...) : ")
+                category = input(" category (Fashion👔 , Sports⚽ , Education📚 , etc🎨...) : ")
                 description = input("Descibe shortly🧐 : ")      
                 amount = float(input("What was the price 💲 ? : "))
 
                 expense = {
-                        "date": date,
-                        "type": type,
-                        "description": description,
-                        "amount": amount
-                }
+                                        "date": date,
+                                        "category": category,
+                                        "description": description,
+                                        "amount": amount
+                                }
 
-        
+                        
                 ExpenseList.append(expense)
                 print("\n Expense Added Successfully✅ ..")
 
 #2 View Your Expense
         elif choice == 2:
-                if len(ExpenseList) == 0:
-                        print("You have To add an Expense")
+                if not  ExpenseList:
+                                print("No Exxpenses Added yet")
                 else:
-                        print("Your expenses💸 are as follows 👇:")
-                        count = 1
-                        for items in ExpenseList:
-                                print(f"Expense no.{count}:\n{items['date']},\n{items['type']},\n{items['description']},\n{items['amount']}\n ---------------")
-                                count = count+1
+                        print("\n Your Expense is as follows 👇:")
+                        for i , item in enumerate(ExpenseList,start=1):
+                                        print(f"""
+
+Expense no. {i}
+Date : {item["date"]}
+Category : {item["category"]}
+Description : {item["description"]}
+Amount : ${item["amount"]}
+""")
 
 #3 View Total Spending
         elif choice == 3:
-                total = 0
-                for items in ExpenseList:
-                        total = total + items["amount"]
-                        
-                print(f"Total Spend 🤑: \n{total}")
+                total = sum(item["amount"] for item in ExpenseList)
+                print(f"Total Spend 🤑: 💲{total}")
 
 #4Exit 
         elif choice == 4:
                 print("Thanks For using | Bye !!")
                 break
 
-#Any issue
+        #Any issue
         else:
                 print("Invalid Choice | Please Pick Between (1-4)")
                 print("Thanks")
